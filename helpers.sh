150d932f76244210f9329cf0844526ea7b8ef7cf
LilyRose2798 <lily@rose.place>
2024-03-23 00:15:00 +1100

    Add helper shell script for downloading and submitting

---
+++ helpers.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+export TRACK="gleam"
+
+download() {
+    if [ -z "$1" ]; then echo 'Usage: download <EXERCISE>'; return 1; fi
+    WS=`exercism workspace`
+    export EXERCISE="$1"
+    if [ -d "$WS/$TRACK/$EXERCISE" ]; then echo 'Exercise "'"$EXERCISE"'" already downloaded'; return 1; fi
+    exercism download --track=$TRACK --exercise="$EXERCISE"
+}
+
+attempt() {
+    if [ "$1" ]; then export EXERCISE="$1"; fi
+    if [ -z "$EXERCISE" ]; then echo '$EXERCISE not set and arg not provided'; return 1; fi
+    WS=`exercism workspace`
+    if [ ! -d "$WS/$TRACK/$EXERCISE" ]; then echo 'Exercise "'"$EXERCISE"'" not downloaded yet'; return 1; fi
+    CUR=$PWD
+    cd "$WS/$TRACK/$EXERCISE"
+    gleam test && exercism submit && git add -A && git commit -m "Solve $EXERCISE exercise" && git push
+    cd $CUR
+}
