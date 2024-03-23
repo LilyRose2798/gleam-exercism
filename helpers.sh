46b9e77bbd8e9f74caa47224a1f1f158dadc25a5
LilyRose2798 <lily@rose.place>
2024-03-23 14:43:57 +1100

    Improve helpers script

--- helpers.sh
+++ helpers.sh
@@ -2,21 +2,39 @@
 
 export TRACK="gleam"
 
-download() {
-    if [ -z "$1" ]; then echo 'Usage: download <EXERCISE>'; return 1; fi
+exercism-dl() {
+    if [ "$1" ]; then export EXERCISE="$1"; else; echo 'Usage: download <EXERCISE>'; return 1; fi
     WS=`exercism workspace`
-    export EXERCISE="$1"
-    if [ -d "$WS/$TRACK/$EXERCISE" ]; then echo 'Exercise "'"$EXERCISE"'" already downloaded'; return 1; fi
-    exercism download --track=$TRACK --exercise="$EXERCISE" && code "$WS/$TRACK/$EXERCISE/src/"*".gleam" && code "$WS/$TRACK/$EXERCISE/test/"*".gleam" && code "$WS/$TRACK/$EXERCISE/README.md"
+    EX_DIR="$WS/$TRACK/$EXERCISE"
+    if [ -d "$EX_DIR" ]; then echo 'Exercise "'"$EXERCISE"'" already downloaded'; return 1; fi
+    exercism download --track=$TRACK --exercise="$EXERCISE" && code "$EX_DIR/src/"*".gleam" && code "$EX_DIR/test/"*".gleam" && code "$EX_DIR/README.md"
 }
 
-attempt() {
+exercism-test() {
     if [ "$1" ]; then export EXERCISE="$1"; fi
     if [ -z "$EXERCISE" ]; then echo '$EXERCISE not set and arg not provided'; return 1; fi
     WS=`exercism workspace`
-    if [ ! -d "$WS/$TRACK/$EXERCISE" ]; then echo 'Exercise "'"$EXERCISE"'" not downloaded yet'; return 1; fi
+    EX_DIR="$WS/$TRACK/$EXERCISE"
+    if [ ! -d "$EX_DIR" ]; then echo 'Exercise "'"$EXERCISE"'" not downloaded yet'; return 1; fi
     CUR=$PWD
-    cd "$WS/$TRACK/$EXERCISE"
-    gleam test && exercism submit && git add -A && git commit -m "Solve $EXERCISE exercise" && git push
+    cd "$EX_DIR"
+    gleam test
+    cd $CUR
+}
+
+exercism-attempt() {
+    if [ "$1" ]; then export EXERCISE="$1"; fi
+    if [ -z "$EXERCISE" ]; then echo '$EXERCISE not set and arg not provided'; return 1; fi
+    WS=`exercism workspace`
+    EX_DIR="$WS/$TRACK/$EXERCISE"
+    if [ ! -d "$EX_DIR" ]; then echo 'Exercise "'"$EXERCISE"'" not downloaded yet'; return 1; fi
+    CUR=$PWD
+    cd "$EX_DIR"
+    gleam test && \
+    exercism submit && \
+    git add -A "$EX_DIR" && \
+    git commit -m "Solve $EXERCISE exercise" && \
+    git push && \
+    export EXERCISE=""
     cd $CUR
 }
